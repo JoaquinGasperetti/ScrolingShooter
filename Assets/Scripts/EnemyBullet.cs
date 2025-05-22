3373a6fcf3824a132d7b2f00a71156be556b0147
JoaquinGasperetti <143657143+JoaquinGasperetti@users.noreply.github.com>
2025-05-21 21:25:45 -0300

    asd

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    public float speed = 10f;
     public float lifetime = 5f;
 
     void Start()
@@ -9,11 +10,16 @@ public class EnemyBullet : MonoBehaviour
         Destroy(gameObject, lifetime);
     }
 
+    void Update()
+    {
+        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            
             Destroy(gameObject);
         }
     }
